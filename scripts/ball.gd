fad9e413e76ad4d3b9fe5b5b122db5c0cfb0e786
Joel J Gerard <git@jjg.dev>
2025-09-20 13:33:29 -0700

    Update ball.gd
    Rotate the ball, Include delta in move_and_collide calculations per docs, modify speed variables to account for delta inclusion. Implemented function that temporary disables collision with ball and paddle, calling after collision.

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,12 +1,14 @@
 extends PhysicsBody2D
 
-var speed = 1 #initial speed of the ball
-var speedMod = 0.5 #speed modifier, used for logarithmic speed increase
+var speed = 100 #initial speed of the ball
+var speedMod = 5 #speed modifier, used for logarithmic speed increase
 var dir : Vector2 = Vector2(1.0, 0.0) #set initial direction, otherwise the ball will just sit there when you press play
 
 var chick = preload("res://scenes/chick.tscn")
+
 func _physics_process(delta):
-	var collision = move_and_collide(dir*speed) #move in a direction, doing physics collision calculations.
+	var collision = move_and_collide(dir * speed * delta) #move in a direction, doing physics collision calculations.
+	rotation_degrees += 90 * delta # Rotates the ball
 	
 	if collision: #if we hit something (egg or paddle right now)
 		dir = dir.bounce(collision.get_normal()) #bounce off the object
@@ -28,3 +30,14 @@ func _physics_process(delta):
 				
 				cspawn.global_position = collision.get_position()
 				get_tree().current_scene.add_child(cspawn)
+		elif collision.get_collider().name == "paddle":
+			disable_collision_with_paddle()
+
+func disable_collision_with_paddle():
+	# Disable collision with layer 1
+	collision_layer = 0
+	collision_mask = 0
+	await get_tree().create_timer(0.5).timeout
+	# Re-enable collision
+	collision_layer = 1
+	collision_mask = 1
