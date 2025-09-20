9f007b92dc5ad178daea1c348af38675813d8dc0
Joel J Gerard <git@jjg.dev>
2025-09-20 14:52:32 -0700

    Modify chick spawn
    Moved to egg.gd, renamed score to pop_egg. Modifications to the chick direction randomness

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -4,7 +4,6 @@ var speed = 100 #initial speed of the ball
 var speedMod = 5 #speed modifier, used for logarithmic speed increase
 var dir : Vector2 = Vector2(1.0, randf_range(-0.3, 0.3)) #set initial direction, rand y
 
-var chick = preload("res://scenes/chick.tscn")
 
 func _physics_process(delta):
 	var collision = move_and_collide(dir * speed * delta) #move in a direction, doing physics collision calculations.
@@ -12,24 +11,16 @@ func _physics_process(delta):
 	
 	if collision: #if we hit something (egg or paddle right now)
 		dir = dir.bounce(collision.get_normal()) #bounce off the object
+		var pos = collision.get_position()
 		
 		if collision.get_collider().name == "Egg": #if we hit the egg, trigger the egg's score function
-			collision.get_collider().score()
+			collision.get_collider().pop_egg(pos, dir)
 			
 			speed +=speedMod #speed up the ball every time it hits the egg
 			speedMod*=0.83 #my relatively bad attempt at logarithmic speed up. Settles out at around 3-4 speed
 			print("Speed: "+ str(speed))
 			
-			for i in 5: #this is all used to spawn the 'chick' particles off of where the ball hits.
-				i+=1
-				var cspawn = chick.instantiate()
-				var rng = RandomNumberGenerator.new()
-				
-				#messy code. Basically, take the ball's bounce vector and randomize it so they all head in the same general direction but don't just follow the ball's path. Explosion!
-				cspawn.dir = Vector2(rng.randf_range(0.2*sign(dir.x),sign(dir.x)),rng.randf_range(0.2*sign(dir.y),sign(dir.y)))
-				
-				cspawn.global_position = collision.get_position()
-				get_tree().current_scene.add_child(cspawn)
+			
 		elif collision.get_collider().name == "paddle":
 			disable_collision_with_paddle()
 
--- scripts/egg.gd
+++ scripts/egg.gd
@@ -1,9 +1,10 @@
 extends StaticBody2D
 
 @onready var gm = $"../GameManager" #get reference to the game manager
-var stages = [1,1,1,1,1,1,1]#[5,3,3,2,2,2,1] #set up stages, each determines how many hits before going to the next.
+var stages = [1,1,1,1,1,1,1]#[5,3,3,2,2,2,1] #set up stages, each determines how many hits before going to the next. 
 var stage = 0 #stage (index of stages)
 var crackCount = 0 
+var chick = preload("res://scenes/chick.tscn")
 
 # Array that holds all colors of egg sprites
 var egg_sprites = [
@@ -19,26 +20,42 @@ var egg_sprites = [
 ]
 
 # Set egg texture to a random egg color.
-func _rnd_egg_color():
+func rnd_egg_color():
 	var rnd_i = randi() % egg_sprites.size() 
 	$Egg_Sprite.texture = load(egg_sprites[rnd_i])
 
 func _ready():
-	_rnd_egg_color()
+	rnd_egg_color()
 
 #slowly grow if egg is less than full size
 #func _physics_process(delta):
 	#if scale < Vector2.ONE:
 		#scale += Vector2(0.0002,0.0002)
 		
-func score():
-	_rnd_egg_color()
+func pop_egg(pos, dir):
+	
 	gm.addScore(1) #increment score for game manager
-	crackCount+=1
+	
 	if stage == stages.size(): #avoid getting an index out of range
 		return
+	
+	crackCount+=1
 	if crackCount >= stages[stage]: #if we've hit the egg enough, move on the the next stage.
 		print("Stage: " + str(stage))
 		stage +=1 #go to the next stage(index of stages)
 		crackCount=0 #reset the hit count
 		scale -= Vector2(0.14,0.14) #shrink the egg
+		rnd_egg_color() # Change color
+	
+	
+	var num_chicks = 2 * (stage + 1) # Spawn the chick particles - progressively more particles spawn as eggs pop
+	for i in num_chicks: #this is all used to spawn the 'chick' particles off of where the ball hits.
+		i+=1
+		var cspawn = chick.instantiate()
+		var rng = RandomNumberGenerator.new()
+		
+		# Basically, take the ball's bounce vector and randomize it so they all head in the same general direction but don't just follow the ball's path. Explosion!
+		cspawn.dir = Vector2(rng.randf_range(0.05*sign(dir.x), 0.30*sign(dir.x)),rng.randf_range(0.05*sign(dir.y), 0.30*sign(dir.y)))
+		
+		cspawn.global_position = pos
+		get_tree().current_scene.add_child(cspawn)
